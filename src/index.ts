e9126ec059e17b3cc0e8503e9c84ffddad643794
Imad Syed <creeperone23@gmail.com>
2025-09-26 22:16:03 -0400

    Appened userRoutes to app

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import productRoutes from './productRoutes'
+import userRoutes from './userRoutes'
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,8 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/products', productRoutes)
+
+app.use('/users', userRoutes)
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
