b675d443425302a6ed85af363a898a70d3864576
Imad Syed <creeperone23@gmail.com>
2025-10-17 00:22:27 -0400

    Moved to prisma postgres

--- prisma/migrations/20250921023844_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Product" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "price" INTEGER NOT NULL,
-    "image" TEXT NOT NULL,
-    "description" TEXT NOT NULL
-);
