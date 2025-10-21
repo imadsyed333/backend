49505fa81d9c46c37eb560709f2faf296451cc51
Imad Syed <creeperone23@gmail.com>
2025-10-21 01:25:57 -0400

    Changed accessToken expiry

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,7 @@ export const ACCESS_COOKIE_OPTIONS = {
     httpOnly: true,
     sameSite: "lax" as const,
     secure: process.env.NODE_ENV === "production",
-    maxAge: 15 * 60 * 1000
+    maxAge: 60 * 60 * 1000
 }
 
 export const REFRESH_COOKIE_OPTIONS = {
