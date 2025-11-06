6eaf4fb925930ae90c42a453fb0ef884728c6502
Imad Syed <creeperone23@gmail.com>
2025-11-06 17:58:12 -0500

    Changed made by the prettier formatter :)

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -4,66 +4,82 @@ import z from "zod";
 import { PrismaClient } from "../../generated/prisma";
 
 export interface AuthRequest extends Request {
-    user?: { id: number, email?: string }
+  user?: { id: number; email?: string };
 }
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const registerSchema = z.object({
-    name: z.string().nonempty({ error: "Must not be empty" }),
-    email: z.email().refine(async (input) => {
-        const count = await prisma.user.count({
-            where: {
-                email: input
-            }
-        })
-        return count < 1
-    }, "Email already exists"),
-    password: z.string().min(8, { error: "Must have at least 8 characters" }).regex(/[a-z]+/, { error: "Must contain a lower-case letter" }).regex(/[A-Z]+/, { error: "Must contain an upper-case letter" }).regex(/[0-9]+/, { error: "Must contain a digit" })
-})
+  name: z.string().nonempty({ error: "Must not be empty" }),
+  email: z.email().refine(async (input) => {
+    const count = await prisma.user.count({
+      where: {
+        email: input,
+      },
+    });
+    return count < 1;
+  }, "Email already exists"),
+  password: z
+    .string()
+    .min(8, { error: "Must have at least 8 characters" })
+    .regex(/[a-z]+/, { error: "Must contain a lower-case letter" })
+    .regex(/[A-Z]+/, { error: "Must contain an upper-case letter" })
+    .regex(/[0-9]+/, { error: "Must contain a digit" }),
+});
 
 const loginSchema = z.object({
-    email: z.email(),
-    password: z.string(),
-})
+  email: z.email(),
+  password: z.string(),
+});
 
-export const validateRegister = async (req: Request, res: Response, next: NextFunction) => {
-    const result = await registerSchema.safeParseAsync(req.body)
-    if (result.success) {
-        next()
-    } else {
-        const errors = z.flattenError(result.error)
-        console.log(errors)
-        res.status(400).json(errors.fieldErrors)
-    }
-}
+export const validateRegister = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const result = await registerSchema.safeParseAsync(req.body);
+  if (result.success) {
+    next();
+  } else {
+    const errors = z.flattenError(result.error);
+    res.status(400).json(errors.fieldErrors);
+  }
+};
 
-export const validateLogin = (req: Request, res: Response, next: NextFunction) => {
-    const result = loginSchema.safeParse(req.body)
-    if (result.success) {
-        next()
-    } else {
-        res.status(400).json({ error: "Invalid credentials" })
-    }
-}
+export const validateLogin = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const result = loginSchema.safeParse(req.body);
+  if (result.success) {
+    next();
+  } else {
+    res.status(400).json({ error: "Invalid credentials" });
+  }
+};
 
-export const authenticate = async (req: AuthRequest, res: Response, next: NextFunction) => {
-    const authHeader = req.headers.authorization
-    let token: string | undefined
+export const authenticate = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+  let token: string | undefined;
 
-    if (authHeader?.startsWith("Bearer ")) {
-        token = authHeader.split("")[1]
-    } else if (req.cookies?.accessToken) {
-        token = req.cookies.accessToken
-    }
+  if (authHeader?.startsWith("Bearer ")) {
+    token = authHeader.split("")[1];
+  } else if (req.cookies?.accessToken) {
+    token = req.cookies.accessToken;
+  }
 
-    if (!token) return res.status(401).json({ error: "No token provided" })
+  if (!token) return res.status(401).json({ error: "No token provided" });
 
-    try {
-        const payload = verifyAccessToken(token) as { sub: number, email: string }
-        req.user = { id: payload.sub, email: payload.email }
-        next()
-    } catch {
-        return res.status(401).json({ error: "Invalid or expired token" })
-    }
-}
+  try {
+    const payload = verifyAccessToken(token) as { sub: number; email: string };
+    req.user = { id: payload.sub, email: payload.email };
+    next();
+  } catch {
+    return res.status(401).json({ error: "Invalid or expired token" });
+  }
+};
