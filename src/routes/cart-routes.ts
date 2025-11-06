5e535a0d73eb176209e3b9858f43a93740f0d93a
Imad Syed <creeperone23@gmail.com>
2025-11-05 22:25:56 -0500

    Added logic to increment existing cartItem count by given quantity

--- src/routes/cart-routes.ts
+++ src/routes/cart-routes.ts
@@ -20,6 +20,11 @@ const BulkUpdateSchema = z.object({
     )
 })
 
+const CartItemSchema = z.object({
+    productId: z.number(),
+    quantity: z.number().min(1)
+})
+
 // Fetching the current user's cart
 router.get('/all', authenticate, async(req: AuthRequest, res: Response) => {
     try {
@@ -47,24 +52,40 @@ router.post('/add', authenticate, async (req: AuthRequest, res: Response) => {
     try {
         if (!req.user) return res.status(401).json({ error: "Unauthorized" })
         
-        const {productId, quantity} = req.body
-        if (!productId || !quantity) return res.status(400).json({error: "Missing fields"})
+        const parse = CartItemSchema.safeParse(req.body)
+        if (!parse.success) return res.status(400).json(parse.error)
+        
+        const {productId, quantity} = parse.data
 
         const product = await prisma.product.findUnique({
             where: {
-                id: parseInt(productId)
+                id: productId
             }
         })
 
         if (!product) return res.status(404).json({error: `Product with id ${productId} not found`})
         
-        const cartItem = await prisma.cartItem.create({
-            data: {
+        const updateResult = await prisma.cartItem.updateMany({
+            where: {
                 userId: req.user.id,
-                productId: parseInt(productId),
-                quantity: parseInt(quantity),
+                productId,
+            },
+            data: {
+                quantity: {
+                    increment: quantity
+                }
+            }
+        })
+
+        if (updateResult.count === 0) {
+            await prisma.cartItem.create({
+                data: {
+                    userId: req.user.id,
+                    productId,
+                    quantity
                 }
             })
+        }
 
         res.status(201).json({message: "Added to cart"})
 
